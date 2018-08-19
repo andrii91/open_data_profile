fc2803abc1260823fa2743a53117509026176595
Andrii Litvin <al@gogmat.com>
2018-08-19 18:59:10 +0300

    fix

--- src/js/component.js
+++ src/js/component.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     dots: false,
     responsive: {
       0: {
-         loop: true,
+        loop: true,
         items: 1
       },
       800: {
@@ -14,4 +14,66 @@ $(document).ready(function () {
       }
     }
   })
+
+  if ($(window).width() < 1200) {
+    var companyCarousel = $('.company-carousel');
+    companyCarousel.addClass('owl-carousel owl-theme');
+    companyCarousel.owlCarousel({
+      loop: true,
+      margin: 15,
+      nav: true,
+      dots: false,
+      center: true,
+      responsive: {
+        0: {
+          loop: true,
+          items: 2
+        },
+        800: {
+          items: 3
+        }
+      }
+    });
+
+    var coutnCity = 20,
+      countActive = 0,
+      countItem = $('.city-item').length;
+    $('.city-item').addClass('hidden');
+
+    $('.city-item').each(function () {
+      if (coutnCity == countActive) {
+        return false;
+      }
+      $(this).removeClass('hidden');
+      countActive++;
+    });
+
+    $('#more-city').click(function (e) {
+      e.preventDefault();
+
+      coutnCity = coutnCity + 20;
+      if ($('.city-item').hasClass('hidden')) {
+        $('.city-item.hidden').each(function () {
+          if (coutnCity == countActive) {
+            return false;
+          }
+          $(this).removeClass('hidden');
+          countActive++;
+
+        });
+
+      } else {
+        $('#more-city').hide();
+      }
+    })
+  }
+  
+  //чтоб нормально отрабатывала карусель
+  $(window).resize(function(){
+    if($('#company-carousel').hasClass('company-carousel')) {
+      location.reload();
+    }
+  })
+
+
 });
